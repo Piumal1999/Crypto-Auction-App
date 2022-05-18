40a6ef6e8ff9bdc25e27825e218409e3b6682bf6
Konara98 <lakshithakonara3@gmail.com>
2022-05-18 10:33:45 +0530

    Create user bud activity

---
+++ app/src/main/java/com/cryptobid/cryptobid/activities/UserBidActivity.java
@@ -0,0 +1,16 @@
+package com.cryptobid.cryptobid.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cryptobid.cryptobid.R;
+
+public class UserBidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_bid);
+    }
+}
