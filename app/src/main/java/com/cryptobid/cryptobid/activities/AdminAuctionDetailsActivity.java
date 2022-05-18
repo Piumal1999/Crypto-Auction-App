31056f9bbe969ac922023f5c6ce8c2e24dd5ae41
chathurawimalasiri <e18402@eng.pdn.ac.lk>
2022-05-17 23:48:31 -0700

    Create admin auction details activity

---
+++ app/src/main/java/com/cryptobid/cryptobid/activities/AdminAuctionDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.cryptobid.cryptobid.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminAuctionDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_auction_details);
+    }
+}
